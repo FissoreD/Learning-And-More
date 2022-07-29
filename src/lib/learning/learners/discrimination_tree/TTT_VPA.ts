08a09362e5af20c112283c03657d757908da8419
FissoreD <davide.fissore@etu.univ-cotedazur.fr>
2022-07-29 12:49:36 +0200

    Jest ignore reactjs coverage

--- src/lib/learning/learners/discrimination_tree/TTT_VPA.ts
+++ src/lib/learning/learners/discrimination_tree/TTT_VPA.ts
@@ -48,8 +48,6 @@ export default class TTT_VPA extends LearnerFather<DiscTreeVPA, StateVPA> {
         }
       }
     }
-    console.log(this.dataStructure.toDot());
-
     this.makeAutomaton()
   }
 
